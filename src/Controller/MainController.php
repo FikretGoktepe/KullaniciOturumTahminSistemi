46dcbcd1564b0a3257c0c6262b0265a1aa346db9
Fikret Göktpe <Fikret_Goktepe@outlook.com>
2025-07-22 02:39:06 +0300

    Basit Bir Cache Eklendi
    Basit bir json cache ile sunucuya durmadan yük binmesi engellendi ve cevap hızı artırıldı.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,10 +1,28 @@
 <?php
 
 namespace Fikretgoktepe\KullaniciOturumTahminSistemi\Controller;
+
 use Fikretgoktepe\KullaniciOturumTahminSistemi\Services\EstimateTimeService;
 
-class MainController {
-    public static function GetEstimatedTimes(){
-        return EstimateTimeService::CalculateEstimatedTimes();
+class MainController
+{
+    public static function GetEstimatedTimes()
+    {
+        $cacheFile = __DIR__ . '/../LocalStorage/cache.json';
+        $cacheDuration = 30 * 60;
+
+        //Cache dosyası var ve güncel ise cache üzerinden veri döndürülüyor.
+        if (file_exists($cacheFile) && (time() - filemtime($cacheFile) < $cacheDuration)) {
+            return file_get_contents($cacheFile);
+        } else {
+            $result = EstimateTimeService::CalculateEstimatedTimes();
+            if($result['status'] == 1){
+            $jsonResult = json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            file_put_contents($cacheFile, $jsonResult);
+            return $jsonResult;
+            }else{
+                return $result;
+            }
+        }
     }
 }
