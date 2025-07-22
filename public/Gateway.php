ad9f9ed4838d6379d1a8004a25cb2b7016a74933
Fikret Göktpe <Fikret_Goktepe@outlook.com>
2025-07-22 03:25:33 +0300

    CORS
    API erişimi için cors izinleri eklendi

--- public/Gateway.php
+++ public/Gateway.php
@@ -1,6 +1,9 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
 use Fikretgoktepe\KullaniciOturumTahminSistemi\Controller\MainController;
 
 $result = MainController::GetEstimatedTimes();
